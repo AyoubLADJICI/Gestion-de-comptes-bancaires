37ff1cc68066a405dde955d89a60668e2840719b
votre_login <votre_mail>
2024-12-30 02:26:39 +0100

    Debut du projet

---
+++ TCP/client.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "client.h"
+
+static void init(void) {
+    printf("✅ Initialisation du serveur...\n");
+}
+
+static void end(void) {
+    printf("🛑 Fermeture du serveur...\n");
+}
+
+static void client_app(const char *address, const char *name) {
+   SOCKET sock = client_init_connection(address);
+   char buffer[BUF_SIZE];
+
+   fd_set rdfs;
+
+   /* send our name */
+   write_server(sock, name);
+   printf("✅ Nom du client envoyé : %s\n", name);
+   while(1) {
+      FD_ZERO(&rdfs);
+
+      /* add STDIN_FILENO */
+      FD_SET(STDIN_FILENO, &rdfs);
+
+      /* add the socket */
+      FD_SET(sock, &rdfs);
+
+      if(select(sock + 1, &rdfs, NULL, NULL, NULL) == -1) {
+         perror("select()");
+         exit(errno);
+      }
+
+      /* something from standard input : i.e keyboard */
+      if(FD_ISSET(STDIN_FILENO, &rdfs)) {
+         fgets(buffer, BUF_SIZE - 1, stdin);
+         {
+            char *p = NULL;
+            p = strstr(buffer, "\n");
+            if(p != NULL) {
+               *p = 0;
+            }else {
+               /* fclean */
+               buffer[BUF_SIZE - 1] = 0;
+            }
+         }
+         write_server(sock, buffer);
+      }
+      else if(FD_ISSET(sock, &rdfs)) {
+         int n = read_server(sock, buffer);
+         /* server down */
+         if(n == 0) {
+            printf("Server disconnected !\n");
+            break;
+         }
+         puts(buffer);
+      }
+   }
+
+   end_connection(sock);
+}
+
+static int client_init_connection(const char *address) {
+   SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+   SOCKADDR_IN sin = { 0 };
+   struct hostent *hostinfo;
+
+   if(sock == INVALID_SOCKET) {
+      perror("socket()");
+      exit(errno);
+   }
+
+   hostinfo = gethostbyname(address);
+   if (hostinfo == NULL) {
+      fprintf (stderr, "Unknown host %s.\n", address);
+      exit(EXIT_FAILURE);
+   }
+
+   sin.sin_addr = *(IN_ADDR *) hostinfo->h_addr;
+   sin.sin_port = htons(PORT);
+   sin.sin_family = AF_INET;
+
+   printf("🔄 Tentative de connexion au serveur %s sur le port %d...\n", address, PORT);
+   if (connect(sock, (SOCKADDR *)&sin, sizeof(sin)) == SOCKET_ERROR) {
+    perror("❌ Erreur lors de la connexion au serveur");
+    exit(errno);
+   }else {
+    printf("✅ Connexion établie avec le serveur !\n");
+   }
+
+   return sock;
+}
+
+static void end_connection(int sock) {
+   closesocket(sock);
+}
+
+static int read_server(SOCKET sock, char *buffer) {
+   int n = 0;
+
+   if((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0) {
+      perror("recv()");
+      exit(errno);
+   }
+
+   buffer[n] = 0;
+
+   return n;
+}
+
+static void write_server(SOCKET sock, const char *buffer) {
+   if(send(sock, buffer, strlen(buffer), 0) < 0) {
+      perror("send()");
+      exit(errno);
+   }
+}
+
+int main(int argc, char **argv) {
+   if(argc < 2) {
+      printf("Usage : %s [address] [pseudo]\n", argv[0]);
+      return EXIT_FAILURE;
+   }
+
+   init();
+
+   client_app(argv[1], argv[2]);
+
+   end();
+
+   return EXIT_SUCCESS;
+}
